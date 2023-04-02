0c6134bd95dd913e38d77fbc3e9025a0a6f94c54
Iram Fattah <iram.fattah@accesscfa.com>
2023-04-02 11:01:51 -0400

    move RemoteFeedLoader components to public module

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,22 +6,14 @@
 //
 
 import XCTest
+import EssentialFeed
 
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-    init(url: URL, client: HTTPClient) {
-        self.client = client
-        self.url = url
+private class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+
+     func get(from url: URL) {
+        requestedURL = url
     }
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
@@ -43,7 +35,7 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
 
     // MARK: Helpers
-
+    
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut:RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
@@ -51,12 +43,5 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     }
 
-    private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
-
-         func get(from url: URL) {
-            requestedURL = url
-        }
-    }
 
 }
