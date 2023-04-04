b7775a9d2dd81f048ffb36cd104d33bb13775330
Iram Fattah <iram.fattah@accesscfa.com>
2023-04-04 00:23:56 -0400

    Replace stubbing by using arrays

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,12 +10,11 @@ import EssentialFeed
 
 private class HTTPClientSpy: HTTPClient {
     var requestedURLs = [URL]()
-    var error: Error?
+    var completions = [(Error) -> Void]()
 
     func get(from url: URL, completion: @escaping (Error) -> Void) {
-        if let error = error {
-            completion(error)
-        }
+
+        completions.append(completion)
         requestedURLs.append(url)
     }
 }
@@ -48,9 +47,10 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
         var capturedErrors =  [RemoteFeedLoader.Error]()
+        let clientError = NSError(domain: "Test", code: 0)
         sut.load {capturedErrors.append($0) }
+        client.completions[0](clientError)
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
 
