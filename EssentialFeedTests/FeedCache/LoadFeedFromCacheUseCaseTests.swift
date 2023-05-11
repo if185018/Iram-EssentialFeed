40af443a7cfe38aeba43b165c2f754a6a4e9f4c5
Iram Fattah <iram.fattah@accesscfa.com>
2023-05-11 13:55:22 -0400

    deletes cache on retrieval error

--- EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/LoadFeedFromCacheUseCaseTests.swift
@@ -77,6 +77,14 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         })
     }
 
+    func test_load_deletesCacheOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        sut.load { _ in }
+
+        store.completeRetrieval(with: anyNSError())
+        XCTAssertEqual(store.recievedMessages, [.retrieve, .deleteCacheFeed])
+    }
+
 
 
     // MARK: Helpers
