6560e3290af56ecadf4a288ca229f43691c782fd
Iram Fattah <iram.fattah@accesscfa.com>
2023-05-14 17:55:43 -0400

    separate date extension helpers into different context to clean scope

--- EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
@@ -19,17 +19,19 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
     return (feed, localFeed)
 }
 
- extension Date {
-     private var feedCacheMaxAgeInDays: Int {
-         return 7
-     }
-     func minusFeedCacheMaxAge() -> Date {
-         return adding(days: -feedCacheMaxAgeInDays)
-     }
-    func adding(days: Int) -> Date {
+extension Date {
+    private var feedCacheMaxAgeInDays: Int {
+        return 7
+    }
+    func minusFeedCacheMaxAge() -> Date {
+        return adding(days: -feedCacheMaxAgeInDays)
+    }
+    private func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
+}
 
+extension Date {
     func adding(seconds: TimeInterval) -> Date {
         return self + seconds
     }
