ca4e16040b42771ed6c4be822a7ebd11c7a099b4
Iram Fattah <iram.fattah@accesscfa.com>
2023-05-18 01:00:03 -0400

    No side effects on retrieving from non empty cache twice

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -136,6 +136,36 @@ class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1)
     }
 
+    func test_retrieveHasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for cache retrieval")
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+            sut.retrieve { firstResult in
+                sut.retrieve { secondResult in
+                    switch (firstResult, secondResult) {
+                    case let (.found(firstFound), .found(secondFound)):
+                        XCTAssertEqual(firstFound.feed, feed)
+                        XCTAssertEqual(firstFound.timestamp, timestamp)
+
+                        XCTAssertEqual(secondFound.feed, feed)
+                        XCTAssertEqual(secondFound.timestamp, timestamp)
+
+
+
+                    default:
+                        XCTFail("Expected retrieving twice from non empty cache to deliver same found result")
+                    }
+                    exp.fulfill()
+                }
+            }
+        }
+
+        wait(for: [exp], timeout: 1)
+    }
+
 
     // MARK: Helpers
 
