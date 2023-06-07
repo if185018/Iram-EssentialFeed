d4a34ecacc08838c415ce5bc4d792b28c6013ba5
Iram Fattah <iram.fattah@accesscfa.com>
2023-06-07 14:44:48 -0400

    add FeedCache Integration Test scheme

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Fattah, Iram on 6/7/23.
+//
+
+import XCTest
+
+ class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+
+
+}
