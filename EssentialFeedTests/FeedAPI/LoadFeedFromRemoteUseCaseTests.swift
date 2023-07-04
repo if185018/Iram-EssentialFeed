80078a5c60d9d153c75290c339d308b2fdeea46f
Iram Fattah <iram.fattah@accesscfa.com>
2023-07-04 16:02:52 -0400

    Refactor HTTPClientResult into HTTPClient.Result

--- EssentialFeedTests/FeedAPI/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/FeedAPI/LoadFeedFromRemoteUseCaseTests.swift
@@ -9,13 +9,13 @@ import XCTest
 import EssentialFeed
 
 private class HTTPClientSpy: HTTPClient {
-    private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+    private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
 
     var requestedURLs: [URL] {
         return messages.map { $0.url }
     }
 
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
         messages.append((url, completion))
     }
 
@@ -29,7 +29,7 @@ private class HTTPClientSpy: HTTPClient {
                                        httpVersion: nil,
                                        headerFields: nil
         )!
-        messages[index].completion(.success(data, response))
+        messages[index].completion(.success((data, response)))
     }
 
 }
