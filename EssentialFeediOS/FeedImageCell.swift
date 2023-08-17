ac4644fab3f08f20fd6db1d9e8dbdf7461cb44e2
Iram Fattah <iram.fattah@accesscfa.com>
2023-08-17 11:04:38 -0400

    Test load feed completion renders successful feed

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Fattah, Iram on 8/15/23.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel() 
+    
+}
+
