5295a99ffcf6c885f96b45f8ea544e5dd7dcc4df
Iram Fattah <iram.fattah@accesscfa.com>
2023-08-31 02:24:24 -0400

    extract feedloader logic from feedview controller to new refreshfeedviewcontroller

---
+++ EssentialFeediOS/Controllers/FeedRefreshViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FeedRefreshViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Fattah, Iram on 8/31/23.
+//
+
+import Foundation
---
+++ EssentialFeediOS/FeedImageDataLoader/FeedImageDataLoader.swift
@@ -0,0 +1,8 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Fattah, Iram on 8/31/23.
+//
+
+import Foundation
